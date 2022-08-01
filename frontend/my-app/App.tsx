8ddd58162f0fb38b73e1f8c12416cb3c59d0cb02
Talia <amsalem.talia@gmail.com>
2022-08-01 23:19:27 +0300

    screens update

--- frontend/my-app/App.tsx
+++ frontend/my-app/App.tsx
@@ -1,6 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StatusBar } from 'expo-status-bar';
+import {FC} from 'react';
+import { StyleSheet, Text, View, Image, TouchableHighlight, Button } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+/*
 export default function App() {
   return (
     <View style={styles.container}>
@@ -8,13 +13,23 @@ export default function App() {
       <StatusBar style="auto" />
     </View>
   );
-}
+}*/
+
+const App:FC = () => {
+  return(
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  )
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+export default App;
